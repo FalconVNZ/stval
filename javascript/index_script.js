fe38ccc0d372f06c39235f7e2fdf433384645a67
FalconVNZ <102573866+FalconVNZ@users.noreply.github.com>
2024-02-12 13:15:25 +0100

    Update index_script.js

--- javascript/index_script.js
+++ javascript/index_script.js
@@ -9,6 +9,14 @@ btn2.addEventListener('mouseover', () => {
     btn2.style.top = `${randomY}px`;
 });
 
+btn2.addEventListener('click', () => {
+    const randomX = Math.floor(Math.random() * (container.offsetWidth - btn2.offsetWidth));
+    const randomY = Math.floor(Math.random() * (container.offsetHeight - btn2.offsetHeight));
+    btn2.style.position = 'absolute';
+    btn2.style.left = `${randomX}px`;
+    btn2.style.top = `${randomY}px`;
+});
+
 const btn1 = document.querySelector('.btn1');
 
 btn1.addEventListener('click', () => {
