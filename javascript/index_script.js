4ae08687f3e72906c608fdcdd0625b48ccf5cd6b
FalconVNZ <102573866+FalconVNZ@users.noreply.github.com>
2024-02-12 12:56:35 +0100

    Add files via upload

---
+++ javascript/index_script.js
@@ -0,0 +1,19 @@
+const btn2 = document.querySelector('.btn2');
+const container = document.querySelector('.container');
+
+btn2.addEventListener('mouseover', () => {
+    const randomX = Math.floor(Math.random() * (container.offsetWidth - btn2.offsetWidth));
+    const randomY = Math.floor(Math.random() * (container.offsetHeight - btn2.offsetHeight));
+    btn2.style.position = 'absolute';
+    btn2.style.left = `${randomX}px`;
+    btn2.style.top = `${randomY}px`;
+});
+
+const btn1 = document.querySelector('.btn1');
+
+btn1.addEventListener('click', () => {
+    alert('Ya sabía que me querias ❤️');
+    setTimeout(() => {
+        window.location.href = './imagenes/carta/carta.pdf';
+    }, 2000);
+});
